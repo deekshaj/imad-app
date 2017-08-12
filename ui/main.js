a63a93c0fcc6b38d676e8bc2f37d69b422eeb174
deekshaj <30804997+deekshaj@users.noreply.github.com>
2017-08-12 10:52:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,15 @@ elt.innerHTML='welcome to javascript'
 //to move the image
 
 var img=document.getElementById('img');
-img.onclick=function(){
+var marginLeft=0; //declaring marginLeft variable
+//function moveRight
+function moveRight()
+{
+    marginLeft=marginLeft+10;  //increment by 10
+    img.style.marginLeft=marginLeft+'px';   //merge the 10 with string 
     
-img.style.marginLeft='100px';    
+};
+img.onclick=function(){
+    var interval=setInterval(moveRight,200);
+//img.style.marginLeft='100px';    
 };
