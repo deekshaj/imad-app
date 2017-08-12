c5c679ef589e607864a451b1f2c1b423bcabffb2
deekshaj <30804997+deekshaj@users.noreply.github.com>
2017-08-12 10:41:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,12 @@ console.log('Loaded!');
 //change the text of the div element
 var elt=document.getElementById('main-element');
 elt.innerHTML='welcome to javascript'
+
+
+//to move the image
+
+var ims=document.getElementById('img');
+ims.onclick=function(){
+    
+img.style.marginleft="100px";    
+};
