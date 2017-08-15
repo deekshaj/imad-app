72a384db4df3be3911ea0a934d8a1ba89b6601ba
deekshaj <30804997+deekshaj@users.noreply.github.com>
2017-08-15 18:02:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-//creating database pool
-var Pool=require('pg').Pool;
-
-//creating configuration for database
-var config={
-    user:'deekshamoily',
-    database:'deekshamoily',
-    host:'db.imad.hasura-app.io',
-    port: '5432',
-    password:process.env.DB-PASSWORD//this line says use the environment code called db-password
-};
 
 
 var app = express();
@@ -101,25 +90,6 @@ function createtemplate(data)
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-//create a pool globally so the its for lifetime
-//last as long a your app id running
-var pool=new Pool(config);
-
-app.get('/test-db',function(req,res){
-    //make a select request
-    //retuen a response with results
-    
-   pool.query('SELECT * FROM test',function(err,result){
-       if (err)
-       {
-           res.status(500).send(err.toString());
-       }else
-      {
-          res.send(JSON.stringify(result));
-      }
-   });
 });
 
 
