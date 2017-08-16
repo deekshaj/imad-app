f8553bcd304f50ad6f662c5501143a25ab556af7
deekshaj <30804997+deekshaj@users.noreply.github.com>
2017-08-16 11:35:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config={
+    user:'deekshamoily',
+    database:'deekshmoily',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+};
+
 
 
 var app = express();
@@ -93,11 +104,25 @@ app.get('/', function (req, res) {
 });
 
 
+var pool=new Pool(config);
 
-
-app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/test-db',function(req,res){
+ //make a select request
+ //return a response with results
+     pool.query('SELECT * FROM test',function(err,result){
+       if (err)
+       {
+           res.status(500).send(err.toString());
+       }else
+      {
+          res.send(JSON.stringify(result));
+      }
+   });
 });
+
+
+
+
  var counter=0;
  
 app.get('/counter',function(req,res){
